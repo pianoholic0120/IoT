2f49a2133ef2168cc39748c636fc2a8a803d4810
ChiHaoKai <brojih@gmail.com>
2024-12-17 23:44:46 +0800

    Python code finished

--- Data_fitting.py
+++ Data_fitting.py
@@ -1,6 +1,7 @@
 import numpy  as np 
 import solver
 import math as m
+import scipy.stats as stats
 from datetime import datetime
 def heartrate_preprocessor(heartrate, time):
     heartrate = heartrate.astype(float)
@@ -67,35 +68,44 @@ def average(hour, heartrate, start):
 def matrix_processor(heartrate, air):
     data_mat = np.zeros((2,0))
     j = 0
-    for i in range(len(averaage[0])):
-        if(  averaage[2][i] >= 2 and averaage[2][i] <= 6):
+    for i in range(len(heartrate[0])):
+        if(  heartrate[2][i] >= 2 and heartrate[2][i] <= 6):
             while(True):
                 
-                    if(j <= len(averaair[0])-1):
+                    if(j <= len(air[0])-1):
                         #print(i, j)
-                        if(averaair[0][j] == averaage[0][i] and averaair[1][j] == averaage[1][i] and averaair[2][j] == averaage[2][i]-1):
+                        if(air[0][j] == heartrate[0][i] and air[1][j] == heartrate[1][i] and air[2][j] == heartrate[2][i]-1):
                             #print("HELLO")
-                            if(averaage[3][i] != 0):
-                                data_mat = np.append(data_mat, [[averaage[3][i]], [averaair[3][j]]], axis=1)
+                            if(heartrate[3][i] != 0):
+                                data_mat = np.append(data_mat, [[heartrate[3][i]], [air[3][j]]], axis=1)
                             break
                         else: j += 1
-                    if(j >= len(averaair[0])-1): break
+                    if(j >= len(air[0])-1): break
                 
     return data_mat
 
+def fitter(hdatapath, airdatapath):
+    data = np.genfromtxt(hdatapath, dtype=str, delimiter = ',', skip_header=1)
+    heartrate = data[:,1]
+    time = data[1:,0]   
+    averaage = heartrate_preprocessor(heartrate, time)
+    
+    airdata = np.genfromtxt(airdatapath,dtype=str, encoding = 'utf-8' , delimiter = ',', skip_header=1)
+    pm = airdata[:,4]
+    #pm = pm.astype(float)
+    time = airdata[:,1]
+    averaair = air_preprocessor(pm, time)
+    data_mat = matrix_processor(averaage, averaair)
+    
+    a,b = solver.Linear_LS_Regression(data_mat[1], data_mat[0], len(data_mat[0]))
+            
+    #print(a,b)
+    cor = np.corrcoef(data_mat[1], data_mat[0])
+    cor = cor[0][1]
+    _, p = stats.pearsonr(data_mat[1], data_mat[0])
+    return a, b, cor, p
 
-data = np.genfromtxt('./watch_data/merged_and_sorted_with_filled_timestamps.csv',dtype=str, delimiter = ',', skip_header=1)
-heartrate = data[:,1]
-time = data[1:,0]   
-averaage = heartrate_preprocessor(heartrate, time)
-
-airdata = np.genfromtxt('./air_quality/combined.txt',dtype=str, encoding = 'utf-8' , delimiter = ',', skip_header=1)
-pm = airdata[:,4]
-#pm = pm.astype(float)
-time = airdata[:,1]
-averaair = air_preprocessor(pm, time)
-data_mat = matrix_processor(averaage, averaair)
-
-a,b = solver.Linear_LS_Regression(data_mat[1], data_mat[0], len(data_mat[0]))
-        
-print(a,b)
+data_air_path = './air_quality/combined.txt'
+data_h_path = './watch_data/merged_and_sorted_with_filled_timestamps.csv'
+m, b, cor, p = fitter(data_h_path, data_air_path)
+print(m, b, cor, p)
